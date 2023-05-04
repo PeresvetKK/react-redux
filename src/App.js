f3de2f7f4a20325ea1310b376bdee3752da7943a
PeresvetKK <Koltyrinpk@gmail.com>
2023-05-04 23:17:56 +0400

    useDispatch() и useSelector()

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setCount } from "./reducers/reposReducer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const dispatch = useDispatch()
+    // useSelector - позволяет получать какие-либо данные из состояния
+    const count = useSelector(state => state.repos.count)
+
+    function onCountClick() {
+        // вызываем dispatch, в который передаём функцию setCount, которая будет
+        // возвращать action и передадим в неё какое-либо число
+        dispatch(setCount(5))
+    }
+
+    return (
+        <div className="app">
+            {/* при нажати на кнопку изменяем count */}
+            <button onClick={() => onCountClick()}>COUNT</button>
+            <div className="">{count}</div>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/reducers/reposReducer.js
@@ -0,0 +1,32 @@
+// выносим название типа в переменную
+const SET_COUNT = "SET_COUNT";
+
+// дефолтное состояние
+const defaultState = {
+    // полученные репозитории
+    items: [],
+    // true, когда данные получаются с API git hub
+    isFetching: true,
+    count: 0,
+};
+
+// редюсер, отвечающий за репозитории принимает состояние и action
+export default function reposReducer(state = defaultState, action) {
+    switch (action.type) {
+        // обрабатывает данный action
+        case SET_COUNT:
+            return{
+                ...state,
+                count: action.payload 
+            }
+        default:
+            return state;
+    }
+}
+
+// функция, которая возвращает action, т.е. объект, у которого есть поле type и
+// какие-либо данные если захотим изменить значение count в редюсере, нам
+// необходимо будет вызвать функцию setCount, в которую параметром мы передадим
+// число, на которое хотим заменить значение. Функция вернет action, в котором
+// payload будет = тому числу, которое мы передадим в параметры
+export const setCount = (count) => ({ type: SET_COUNT, payload: count });
